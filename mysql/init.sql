9c82fc93174b7f5f54b071db3e777756c176a0ce
root <root@ip-172-31-3-129.ap-southeast-2.compute.internal>
2026-06-24 10:12:48 +0000

    Merge remote changes

---
+++ mysql/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+INSERT INTO employees (name) VALUES ('Hariom');
+INSERT INTO employees (name) VALUES ('DevOps Engineer');
